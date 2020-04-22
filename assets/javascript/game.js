b5b4813ce015446b7fedfd438765030f59dd9e8c
skeown13 <code@keown.net>
2020-04-22 13:31:05 -0700

    starter psuedocode created

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// create Variable to store wins, current word, current guess, letters already guessed, etc
+
+// create Variables as placeholders for where we will put to the html
+
+// create an array for possible words to be the word that the user is guessing
+
+// when first time button is pushed set out the layout of the current word
+
+// if a letter in the current word is pressed replace the placeholder in the layout with the correct letter in the correct space (loop through?)
+
+// every time a letter is pressed decrease teh number of guesses by 1
+
+// when a letter is pressed add that letter to the letters already guessed array
+
+// if current word is guessed increment wins by 1
+
+// 
